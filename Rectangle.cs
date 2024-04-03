d0d156bf28ae826049ab93768968aa2104e7e8f3
VitaliiPachkov <pachkov2006@mail.ru>
2024-04-03 18:16:07 +0400

    Task-02: Added rectangle.cs

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClass
+{
+    public class Rectangle
+    {
+        public double CalculateAreaRectangle(double x, double y)
+        {
+            double result = x * y;
+            return result;
+        }
+
+
+        public double CalculatePerimetrRectangle(double x, double y)
+        {
+            double result = 2 * x + 2 *y;
+            return result;
+        }
+    }
+}
